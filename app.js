bba7c451ca6f26554cd11357040fae62f6827160
Zoi <zoilanbond@gmail.com>
2023-10-06 16:25:58 -0700

    Step 7

--- app.js
+++ app.js
@@ -29,45 +29,81 @@
 var number = 10
 var name = prompt('What is your name?');
 var playAgain = true;
-var response = '';
-var responseTwo = true;
+var playAgainResponse = '';
+//var responseTwo = true;
 var guess = parseInt(prompt("Guess a number between 1 and 100."))
 var solved = false
 var guesses = [];
 
-while (playAgain === true) {
+var playedBefore = false;
 
+
+
+var previousScore = 0;
+
+//Record number of guesses by a player
+
+
+while (playAgain === true) {
+    
     while(solved === false) {
+        //Evaluating guess to random number
         if(number === guess) {
             guesses.push(guess);
+            
             alert(`Good job ${name} it took you ${guesses.length} guesses.`);
             solved = true;
         } else if(number < guess) {
             guesses.push(guess);
+           
             guess = parseInt(prompt(`Sorry ${name}, Lower`));
         } else {
             guesses.push(guess);
+           
             guess = parseInt(prompt(`Sorry ${name}, Higher`));
         }
     }
     
-    alert(`Correct ${name} your previous guesses were ${guesses}`);
+    
+    if (playedBefore === false) {
+        alert(`Correct ${name} your previous guesses were ${guesses}.`);
+        //player has played before
+        playedBefore = true;
+        //record best score
+        //best score will record least amount of guesses
+        previousScore = guesses.length;
+        continue;
+    } else if (previousScore > guesses.length) {
+            alert(`Thats Correct ${name}, you did better in your last game by ${previousScore - guesses.length}.`)
+            previousScore = guesses.length;  
+    } else if (previousScore <= guesses.length){
+            alert(`You can do better`);
+        }
+    
     
     
      while (playAgain === true) {
-        response = prompt(`${name}, do you want to play again`);
-        if(response == 'Yes') {
+        playAgainResponse = prompt(`${name}, do you want to play again`);
+        if(playAgainResponse === 'Yes') {
+            //reset game variables
             solved = false;
             guesses = [];
             guess = parseInt(prompt("Guess a number between 1 and 100."));
             break; 
-        } else if (response == 'No') {
-            playAgain = false
+        } else if (playAgainResponse === 'No') {
+            playBefore = false;
+            name = prompt('What is your name?');
+            solved = false;
+            guesses = [];
+            previousScore = 0;
+            guess = parseInt(prompt("Guess a number between 1 and 100."));
             break;
         } else {
             alert(`Did not understand`);
         }
     }
+
+    
 
 }
 
